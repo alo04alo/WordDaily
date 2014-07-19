2b438689a63667f96b498cc8a4db4175aec2c740
Tam Mao <sonnv90@gmail.com>
2014-07-19 16:29:19 +0700

    set id for lesson

--- src/com/hako/word/vocabulary/SummaryTab.java
+++ src/com/hako/word/vocabulary/SummaryTab.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.hako.base.Word;
 import com.hako.base.WordHandle;
+import com.hako.word.WordSupport;
 
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +18,7 @@ public class SummaryTab extends ListActivity {
 //      private String[] kanji = {"朝","鞄","日本語","へや","きに", "朝","鞄","日本語","へや","きに", "朝","鞄","日本語","へや","きに", "朝","鞄","日本語","へや","きに"};
 //      private String[] mean = {"buổi sáng","cái cặp","tiếng nhật","căn hộ","đất nước", "buổi sáng","cái cặp","tiếng nhật","căn hộ","đất nước", "buổi sáng","cái cặp","tiếng nhật","căn hộ","đất nước", "buổi sáng","cái cặp","tiếng nhật","căn hộ","đất nước"};
       
-	  private List<Word> words = WordHandle.getListWord(1);
+	  private List<Word> words; 
 	  private String[] hiragana;
 	  private String[] kanji;
 	  private String[] mean;
@@ -25,6 +26,9 @@ public class SummaryTab extends ListActivity {
       @Override
       public void onCreate(Bundle savedInstanceState) {
           super.onCreate(savedInstanceState);
+          
+          int current_lesson = ((WordSupport) getApplication()).current_lesson;
+          words = WordHandle.getListWord(current_lesson);
           hiragana = new String[words.size()];
           kanji = new String[words.size()];
           mean = new String[words.size()];
--- src/com/hako/word/vocabulary/VocabularyArrayAdapter.java
+++ src/com/hako/word/vocabulary/VocabularyArrayAdapter.java
@@ -1,15 +1,12 @@
 package com.hako.word.vocabulary;
 
-import com.hako.utils.GlobalData;
 import com.hako.word.R;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class VocabularyArrayAdapter extends ArrayAdapter<String> {

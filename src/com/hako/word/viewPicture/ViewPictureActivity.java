9ba910a2522368773fb9932d8514214756672d1d
Vuong ND <vuongnd90@gmail.com>
2014-07-28 07:55:04 +0700

    Add effect when click on answer buttons

--- src/com/hako/word/viewPicture/ViewPictureActivity.java
+++ src/com/hako/word/viewPicture/ViewPictureActivity.java
@@ -1,5 +1,6 @@
 package com.hako.word.viewPicture;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Random;
 
@@ -10,11 +11,21 @@ import com.hako.word.MainActivity;
 import com.hako.word.R;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent.PointerCoords;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -39,6 +50,10 @@ public class ViewPictureActivity extends Activity{
 	private ImageView imgWord;	
 	
 	private int count = 0;
+	private int positionTrueAnswer;
+	private int selectedAnswer;
+	
+	private MediaPlayer m;
 		
 	List<Word> words;
 	@Override
@@ -78,32 +93,36 @@ public class ViewPictureActivity extends Activity{
 		btnAnswer1.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
-			public void onClick(View v) {		
-				loadNewQuestion();
+			public void onClick(View v) {	
+				selectedAnswer = 1;
+				handleAnswer();
 			}
 		});
 		
 		btnAnswer2.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
-			public void onClick(View v) {			
-				loadNewQuestion();
+			public void onClick(View v) {		
+				selectedAnswer = 2;
+				handleAnswer();		
 			}
 		});
 		
 		btnAnswer3.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
-			public void onClick(View v) {			
-				loadNewQuestion();
+			public void onClick(View v) {	
+				selectedAnswer = 3;
+				handleAnswer();
 			}
 		});
 		
 		btnAnswer4.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
-			public void onClick(View v) {			
-				loadNewQuestion();
+			public void onClick(View v) {	
+				selectedAnswer = 4;
+				handleAnswer();
 			}
 		});
 		
@@ -119,8 +138,34 @@ public class ViewPictureActivity extends Activity{
 			
 			@Override
 			public void onClick(View arg0) {
-				Toast.makeText(getApplicationContext(), words.get(count - 1).romaji, Toast.LENGTH_SHORT).show();
-				playAudio(words.get(count - 1).romaji + ".mp3");
+//				Toast.makeText(getApplicationContext(), words.get(count - 1).romaji, Toast.LENGTH_SHORT).show();				
+//				GlobalData.playAudio(getApplicationContext(), words.get(count - 1).romaji);
+				try {
+			        if (m.isPlaying()) {
+			            m.stop();
+			            m.release();
+			            m = new MediaPlayer();
+			        }
+
+			        AssetFileDescriptor descriptor = getAssets().openFd(words.get(count - 1).romaji + ".mp3");
+			        m.setDataSource(descriptor.getFileDescriptor(), descriptor.getStartOffset(), descriptor.getLength());
+			        descriptor.close();
+
+			        m.prepare();
+			        m.setVolume(1f, 1f);
+			        m.setLooping(false);
+			        m.start();
+			    } catch (Exception e) {
+			        e.printStackTrace();
+			    }
+			}
+		});
+		
+		btnShowFuction.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {			
+				
 			}
 		});
 
@@ -144,21 +189,7 @@ public class ViewPictureActivity extends Activity{
 		// increase value of count variable
 		count++;
 	}
-	
-	
-	private void playAudio(String fileName) {
-		MediaPlayer player = new MediaPlayer();
-		try {
-			AssetFileDescriptor assetFile = getAssets().openFd(fileName);		
-			player.setDataSource(assetFile.getFileDescriptor(), assetFile.getStartOffset(), assetFile.getLength());
-			player.seekTo(0);		
-			player.start();
-			Runtime.getRuntime().gc();
-		} catch (Exception e) {
-			// Error handling
-		}
 		
-	}
 	
 	private void showText() {
 		tvWord.setVisibility(View.VISIBLE);
@@ -175,12 +206,12 @@ public class ViewPictureActivity extends Activity{
 		int max = 4;
 		Random random = new Random();
 		
-		int positionTrueAnswer = random.nextInt(max - min + 1) + min;					
+		positionTrueAnswer = random.nextInt(max - min + 1) + min;					
 		setTextForButton(positionTrueAnswer, words.get(count).hiragana);
 		
 		int[] numbers = getRandomThreeNumber(0, GlobalData.WORD_LIMIT - 1, count);		
 		int index = 0;
-		
+		Toast.makeText(getApplicationContext(), numbers[0] + "_" + numbers[1] + "_" + numbers[2], Toast.LENGTH_LONG).show();
 		// set remain answers
 		for (int i = min; i <= max; i++) {
 			if (i != positionTrueAnswer) {
@@ -190,14 +221,12 @@ public class ViewPictureActivity extends Activity{
 		}
 	}
 	
-	private int[] getRandomThreeNumber(int min, int max, int ignore) {
-		
-		Random random = new Random();
+	private int[] getRandomThreeNumber(int min, int max, int ignore) {			
 		
 		int[]  number = new int[3];
-		number[0] = random.nextInt(max - min + 1) + min;
-		number[1] = random.nextInt(max - min + 1) + min;
-		number[2] = random.nextInt(max - min + 1) + min;
+		number[0] = getRandomOneNumber(min, max, ignore);
+		number[1] = getRandomOneNumber(min, max, ignore);
+		number[2] = getRandomOneNumber(min, max, ignore);
 		
 		if ((number[0] != number[1]) && (number[0] != number[2]) && (number[1] != number[2])) {
 			return number;
@@ -205,6 +234,16 @@ public class ViewPictureActivity extends Activity{
 			return getRandomThreeNumber(min, max, ignore);
 		}
 		
+	}
+	
+	private int getRandomOneNumber(int min, int max, int ignore) {
+		Random random = new Random();
+		int number = random.nextInt(max - min + 1) + min;
+		if (number != ignore) {
+			return number;
+		} else {
+			return getRandomOneNumber(min, max, ignore);
+		}
 	}
 	
 	private void setTextForButton(int id, String text) {
@@ -223,6 +262,76 @@ public class ViewPictureActivity extends Activity{
 		default:
 			break;
 		}
+	}
+	
+	private void handleAnswer() {		
+		// if the answer is true
+		if (selectedAnswer == positionTrueAnswer) {
+			switch (selectedAnswer) {
+			case 1:
+				setColorForButton(btnAnswer1, true);
+				break;
+			case 2:
+				setColorForButton(btnAnswer2, true);
+				break;
+			case 3:
+				setColorForButton(btnAnswer3, true);
+				break;
+			case 4:
+				setColorForButton(btnAnswer4, true);
+				break;
+			default:
+				break;
+			}
+			
+			new Handler().postDelayed(new Runnable() {
+				
+				@Override
+				public void run() {	
+					// load new question
+					loadNewQuestion();
+				}
+			}, 1000);
+		}
+		// if the answer is fail
+		else {
+			switch (selectedAnswer) {
+			case 1:
+				setColorForButton(btnAnswer1, false);
+				break;
+			case 2:
+				setColorForButton(btnAnswer2, false);
+				break;
+			case 3:
+				setColorForButton(btnAnswer3, false);
+				break;
+			case 4:
+				setColorForButton(btnAnswer4, false);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	
+	private void setColorForButton(Button button, boolean bool) {
+		AnimationDrawable animation = new AnimationDrawable();
+		if (bool == true) {
+			animation.addFrame(getResources().getDrawable(R.drawable.common_btn_border_true_answer), 200);
+			animation.addFrame(getResources().getDrawable(R.drawable.common_btn_boder_normal_answer), 200);
+			animation.addFrame(getResources().getDrawable(R.drawable.common_btn_border_true_answer), 200);
+			animation.addFrame(getResources().getDrawable(R.drawable.common_btn_boder_normal_answer), 200);
+		} else {
+			animation.addFrame(getResources().getDrawable(R.drawable.common_btn_border_false_answer), 200);
+			animation.addFrame(getResources().getDrawable(R.drawable.common_btn_boder_normal_answer), 200);
+			animation.addFrame(getResources().getDrawable(R.drawable.common_btn_border_false_answer), 200);
+			animation.addFrame(getResources().getDrawable(R.drawable.common_btn_boder_normal_answer), 200);
+		}
+		animation.setOneShot(true);
+		
+		button.setBackgroundDrawable(animation);	
+		animation.start();
+		
 	}
 
 }

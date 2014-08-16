11fe69f16fe4f6765cb2c0ea022c640767762619
Vuong ND <vuongnd90@gmail.com>
2014-08-16 15:09:00 +0700

    create TestResultScreen for Exam3

--- src/com/hako/word/exam/Exam3Activity.java
+++ src/com/hako/word/exam/Exam3Activity.java
@@ -7,11 +7,15 @@ import com.hako.base.Word;
 import com.hako.base.WordHandle;
 import com.hako.utils.GlobalData;
 import com.hako.word.R;
+import com.hako.word.exam.Exam1Activity.MyCountDownTimer;
 import com.hako.word.viewPicture.ViewPictureActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
+import android.provider.Settings.Global;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -29,6 +33,9 @@ public class Exam3Activity extends Activity{
 	private int count = 0;
 	private int positionTrueAnswer;
 	
+	private CountDownTimer timer;
+	private boolean isCounting = false;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -41,7 +48,8 @@ public class Exam3Activity extends Activity{
 		
 		btnQuestion = (Button) findViewById(R.id.exam_3_audio);
 		words = WordHandle.getRandomListWord(GlobalData.current_lesson, GlobalData.WORD_INCLUDE_IMAGE, GlobalData.TEST_LIMIT);
-		
+		GlobalData.currentExam = 3;
+		GlobalData.testData = words;
 		loadNewQuestion();
 
 		btnAnswer1.setOnClickListener(new View.OnClickListener() {
@@ -88,9 +96,13 @@ public class Exam3Activity extends Activity{
 	protected void loadNewQuestion() {
 		if (count == GlobalData.TEST_LIMIT) {
 			// show result screen
-			Toast.makeText(getApplicationContext(), "Waitting Final Screen..... :))", Toast.LENGTH_LONG).show();
+			startActivity(new Intent(Exam3Activity.this, TestResultActivity.class));
 			return;
 		}
+		
+		isCounting = false;
+		// reset all answer buttons
+		resetButton();
 		
 		GlobalData.playAudioFromAsset(this, words.get(count).romaji + ".mp3");
 		// set text for answer buttons
@@ -107,7 +119,7 @@ public class Exam3Activity extends Activity{
 
 		positionTrueAnswer = random.nextInt(number_of_answer) + 1;
 		button = getButtonFromId(positionTrueAnswer);
-		button.setText(words.get(count).hiragana);
+		button.setText(words.get(count).mean_vi);
 
 		int[] numbers = GlobalData.getRandomThreeNumber(0, GlobalData.TEST_LIMIT - 1, count);		
 		int index = 0;
@@ -115,39 +127,34 @@ public class Exam3Activity extends Activity{
 		for (int i = 1; i <= number_of_answer; i++) {
 			if (i != positionTrueAnswer) {
 				button = getButtonFromId(i);
-				button.setText(words.get(numbers[index]).hiragana);
+				button.setText(words.get(numbers[index]).mean_vi);
 				index++;
 			}
 		}
 	}
 
 	private void handleAnswer(int selectedAnswer) {		
-		// result = true if the answer is true else false
-		String answer = null;
-		Button trueButton;
-		Button falseButton;
-		
-		if (selectedAnswer == positionTrueAnswer){
-			trueButton = getButtonFromId(selectedAnswer); 
-			answer = trueButton.getText().toString();
-			GlobalData.setAnimationForButton(this, trueButton, true); // set Animation if correct answer
-		} else {
-			trueButton = getButtonFromId(positionTrueAnswer);
-			falseButton = getButtonFromId(selectedAnswer);
-			answer = falseButton.getText().toString();
-			GlobalData.setAnimationForButton(this, falseButton, trueButton); // set Animation if fail answer
+		if (isCounting == false) {
+			timer = new MyCountDownTimer(1000, 100);
+			timer.start();
+			isCounting = true;
 		}
 		
-		words.get(count-1).choose_answer = answer;
+		String answer = null;
+		Button selectedButton;		
+		resetButton();
+		selectedButton = getButtonFromId(selectedAnswer);
+		selectedButton.setBackgroundResource(R.drawable.btn_answer_selected_background);
+		answer = selectedButton.getText().toString();		
 		
-		new Handler().postDelayed(new Runnable() {
-
-			@Override
-			public void run() {	
-				// load new question
-				loadNewQuestion();
-			}
-		}, 1000);
+		GlobalData.testData.get(count-1).choose_answer = answer;
+	}
+	
+	private void resetButton() {
+		for (int i = 1; i <= 4; i++) {
+			Button btn = getButtonFromId(i);
+			btn.setBackgroundResource(R.drawable.btn_answer_background);
+		}
 	}
 	
 	private Button getButtonFromId(int id){
@@ -164,5 +171,20 @@ public class Exam3Activity extends Activity{
 			return null;
 		}
 	}
+	
+	public class MyCountDownTimer extends CountDownTimer {
+		  public MyCountDownTimer(long startTime, long interval) {
+		   super(startTime, interval);
+		  }
+		 
+		  @Override
+		  public void onFinish() {
+			  loadNewQuestion();
+		  }
+		 
+		  @Override
+		  public void onTick(long millisUntilFinished) {
+		  }
+	 }	
 	
 }
